c2a8a254cfc24cb23de668f36ffc5e713e1f8084
quochuy2405 <huy01682312920@gmail.com>
2022-07-09 17:35:12 +0700

    update table

--- src/components/Constant/ListTreeItemSwitch.tsx
+++ src/components/Constant/ListTreeItemSwitch.tsx
@@ -1,3 +1,4 @@
+import { Table } from '../HPComponents'
 import type { TreeItemSwitch } from '../Interface'
 
 const ListTreeItem: Array<TreeItemSwitch> = [
@@ -6,7 +7,11 @@ const ListTreeItem: Array<TreeItemSwitch> = [
   { parentURL: 'user', opt: '1', body: <>Tree user1</> },
   { parentURL: 'user', opt: '2', body: <>Tree user2</> },
   { parentURL: 'user', opt: '3', body: <>Tree user3</> },
-  { parentURL: 'hello', opt: '1', body: <>Tree hello1</> },
+  {
+    parentURL: 'hello',
+    opt: '1',
+    body: <Table />
+  },
   { parentURL: 'hello', opt: '2', body: <>Tree hello2</> },
   { parentURL: 'hello', opt: '3', body: <>Tree hello3</> }
 ]
